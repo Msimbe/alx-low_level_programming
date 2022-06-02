7989c39fc6e7adef57fabc710fc8566fcacbd49e
Msimbe <allymsimbe5@gmail.com>
2022-06-02 16:21:08 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - my entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
