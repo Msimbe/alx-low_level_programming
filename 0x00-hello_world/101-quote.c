60ab8d2334770498e8cf267dd5937a72c8465b7d
Msimbe <allymsimbe5@gmail.com>
2022-06-02 19:53:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	system("and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n" ,59);
 	return (1);
 }
