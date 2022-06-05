a6c188cea44449a0ec2591f9f21965ad19b7b5bb
Msimbe <allymsimbe5@gmail.com>
2022-06-05 15:01:12 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	char letters;
+
+	n = 0;
+	letter = 'a';
+
+	while (n < 10)
+		putchar((n % 10) + '0');
+	n++;
+	
+	while (letter <= 'f')
+		putchar(letter);
+	letters++;
+
+	putchar('\n');
+
+	return (0);
+}
