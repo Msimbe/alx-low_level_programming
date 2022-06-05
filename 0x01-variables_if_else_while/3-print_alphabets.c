ea8293e0981b696467ab5292f4d13c09b966b355
Msimbe <allymsimbe5@gmail.com>
2022-06-05 14:03:26 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,11 +16,9 @@ int main(void)
 	
 	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 		putchar(alphabets);
-
 	for (alphabets = 'A'; alphabets <= 'Z'; alphabets++)
 		putchar(alphabets);
 
 	putchar('\n');
-
 	return (0);
 }
