5241ba63c5aad77087bd635f40398090d5ef8165
Msimbe <allymsimbe5@gmail.com>
2022-06-05 14:39:52 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char rev_alp
+
+		for (rev_alp = 'z'; rev_alp >= 'a'; rev-alp--)
+			putchar(rev_alp);
+
+	putchar('\n');
+
+	return 0;
+}
