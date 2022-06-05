178319e385125746770c7993249eaaad98134cff
Msimbe <allymsimbe5@gmail.com>
2022-06-05 13:26:02 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,8 +14,8 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++;)
-		putchar(alphabet
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
 	putchar('\n');
 
 	return (0);
