7404995f5d37d7dd1f2f419d6950e122c29d59b4
Msimbe <allymsimbe5@gmail.com>
2022-06-05 13:23:32 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,13 +14,9 @@ int main(void)
 {
 	char alphabet;
 
-	alphabet = 'a';
-	while (alphabet <= 'z')
-	{
-		putchar(alphabet);
-	alphabet++;
-	putchar('');
-	}
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++;)
+		putchar(alphabet
+	putchar('\n');
 
 	return (0);
 }
