e4b8e45eec0dc8c75cca5bb4bfd81f82d0cf17bb
Msimbe <allymsimbe5@gmail.com>
2022-06-06 09:18:05 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,17 +8,13 @@
 int main(void)
 {
 	int num1,num2;
-	num1 = 0;
-	num2 = 0;
 
-	while(num1 < 10)
+	for (num1 = 0; num2< 10; num2++)
 	{
-		while(num2 < 2)
+		for (num2 = 0; num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
-			num1++;
-			num2++;
 
 			if (num1 == 9 && num2 == 9)
 				continue;
